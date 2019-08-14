974d86f4f7a9cc501017074df80df8767d726ed6
joebelton <joebelton@live.co.uk>
2019-08-14 20:03:18 +0100

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name = "")
+        @name = name
+        #@songs = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select { |s| s.artist == self}
+    end
+
+    def add_song(song)
+        #self.songs << Song.artist
+        song.artist = self
+    end
+
+    def add_song_by_name(name)
+        #@songs << name
+        song = Song.new(name)
+        song.artist = self
+    end
+
+    def self.song_count
+        Song.all.length
+    end
+end
